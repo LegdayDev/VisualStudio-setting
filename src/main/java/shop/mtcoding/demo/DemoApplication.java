30f17791b9453a800e9093759b0928524d2bcd6f
JaeYoung <chlwodud0327@naver.com>
2023-01-03 09:10:23 +0900

    Material Icon Tehme , Jetbrains Fleet Theme 패키지 설치

--- src/main/java/shop/mtcoding/demo/DemoApplication.java
+++ src/main/java/shop/mtcoding/demo/DemoApplication.java
@@ -3,9 +3,6 @@ package shop.mtcoding.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.Getter;
-import lombok.Setter;
-
 @SpringBootApplication
 public class DemoApplication {
 
